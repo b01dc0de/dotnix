d043c09cd65b07d474093fc647b2d2ebb6026425
CKA <163610791+b01dc0de@users.noreply.github.com>
2025-02-01 21:47:33 -0600

    Add new host ether (ThinkPad E14)

---
+++ hosts/ether/default.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+
+{
+    imports = [
+        ./hardware-configuration.nix
+	../../configuration.nix
+    ];
+
+    # Hostname:
+    networking.hostName = lib.mkForce "ether";
+}
